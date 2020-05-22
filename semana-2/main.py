5b8d0945a8ecb1868d3ad0014877fd3d93e25f21
Lucas Henrique Souza <64167295+lucashenrs@users.noreply.github.com>
2020-05-22 15:23:15 -0300

    Att main -abstract classes

--- semana-2/main.py
+++ semana-2/main.py
@@ -1,31 +1,17 @@
 from abc import ABC, abstractmethod
 
-class Department:
+class Department(ABC):
     def __init__(self, name, code):
-        self.name = name
-        self.code = code
+        self.__department_name = name
+        self.__department_code = code
 
-class Employee(ABC):
-    '''Esta é a classe mãe e 
-    não pode ser instanciada diretamente
-    '''
-    WORKING_HOURS = 8
-    BONUS = 0.15
+    def get_department(self):
+            '''Retorna o nome do departamento do empregado.'''
+            return self.__department_name
     
-    @classmethod
-    def get_hours(self):   
-        '''Retorna a carga horária diária'''
-        return Employee.WORKING_HOURS
-
-    def __init__(self, code, name,
-                salary, department): # Método construtor
-        self.name = name
-        self.salary = salary
-        self.__department = department
-
-    def get_departament(self):
-        '''Retorna o nome do departamento do empregado.'''
-        return self.__department.name
+    def get_departament_code(self):
+            '''Retorna o código do departamento do empregado.'''
+            return self.__department_code
     
     def set_departament(self, new_dept, new_dept_code):
         '''Atualiza o departamento do empregado.
@@ -33,14 +19,35 @@ class Employee(ABC):
             -new_dept = nome do departamento 
             -new_dept_code = codigo do departamento
         '''
-        self.__department.name = new_dept
-        self.__department.code = new_dept_code
-
+        self.__department_name = new_dept
+        self.__department_code = new_dept_code
+    
     @abstractmethod
     def calc_bonus(self):
         pass
 
-class Manager(Employee):
+class Employee(ABC):
+    '''Esta é a classe mãe e 
+    não pode ser instanciada diretamente
+    '''
+    BONUS = 0.15
+    
+    def __init__(self, code, name,
+                salary):
+        self.name = name
+        self.salary = salary
+        #self.__department = Department()
+        self.__working_hours = 8
+    
+    def get_hours(self):   
+        '''Retorna a carga horária diária'''
+        return self.__working_hours
+    
+    @abstractmethod
+    def calc_bonus(self):
+        pass
+
+class Manager(Employee, Department):
 
     """Classe Manager, define objeto do departamento 'managers'
 
@@ -49,11 +56,11 @@ class Manager(Employee):
         - name
         - salary
     """
-    def __init__(self, code, name, salary): # Método construtor
+    def __init__(self, code, name, salary):
         Employee.__init__(
-            self, code, name, salary,
-            department= Department('managers', 1))
-
+            self, code, name, salary)
+        Department.__init__(self, 'managers', 1)
+        
     def calc_bonus(self):
         '''Calcula bonus com base no salário
             Bonus = 0.15 * Salário
@@ -61,7 +68,7 @@ class Manager(Employee):
         return self.salary * Employee.BONUS
 
 
-class Seller(Employee):
+class Seller(Employee, Department):
     """Classe Seller, define objeto do departamento 'sellers'
 
     Argumentos:
@@ -70,9 +77,11 @@ class Seller(Employee):
         - salary
     """
 
-    def __init__(self, code, name, salary): # Método construtor
+    def __init__(self, code, name, salary):
         Employee.__init__(self, code, name, 
-        salary, department= Department('sellers', 2))
+        salary)
+        Department.__init__(self, 'sellers', 2)
+        
         self.__sales = 0
     
     def get_sales(self):
